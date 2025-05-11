020de202fba91ac850f1d92a0980bfc1dbadd313
Iryna-Matiash <165329434+Iryna-Matiash@users.noreply.github.com>
2025-05-11 16:37:46 +0300

    update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,32 +1,62 @@
-//import { useState } from "react";//
+import { useState, useEffect } from "react";
 
-import Profile from "../Profile/Profile";
-import userData from "../userData.json";
-import FriendList from "../FriendList/FriendList";
-import friends from "../friends.json";
-import TransactionHistory from "../TransactionHistory/TransactionHistory";
-import transactions from "../transactions.json";
-import Work from "../Work/Work";
+import Description from "../Description/Description";
+import css from "./App.module.css";
+import Options from "../Options/Options";
+import Feedback from "../Feedback/Feedback";
+import Notification from "../Notification/Notification";
 
 export default function App() {
-  return (
-    <>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        followers={userData.stats.followers}
-        views={userData.stats.views}
-        likes={userData.stats.likes}
-      />
-      <>
-        <p>Selected language: </p>
-        <Work />
-      </>
+  const info = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
 
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
-    </>
+  const [data, setData] = useState(() => {
+    const savedData = window.localStorage.getItem("saved-data");
+    console.log(savedData);
+
+    if (savedData !== null) {
+      return JSON.parse(savedData);
+    }
+
+    return info;
+  });
+
+  const updateFeedback = (feedbackType) => {
+    setData({
+      ...data,
+      [feedbackType]: data[feedbackType] + 1,
+    });
+  };
+
+  const resetFeedback = () => {
+    setData(info);
+  };
+
+  const totalFeedback = data.good + data.neutral + data.bad;
+
+  const positive = Math.round((data.good / totalFeedback) * 100);
+
+  useEffect(() => {
+    console.log(data);
+    window.localStorage.setItem("saved-data", JSON.stringify(data));
+  }, [data]);
+
+  return (
+    <div className={css.container}>
+      <Description />
+      <Options
+        value={data}
+        onBtnClick={updateFeedback}
+        resetFeedback={resetFeedback}
+      />
+      {data.good || data.neutral || data.bad !== 0 ? (
+        <Feedback value={data} total={totalFeedback} posAmount={positive} />
+      ) : (
+        <Notification />
+      )}
+    </div>
   );
 }
